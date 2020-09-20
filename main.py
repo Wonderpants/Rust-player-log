112eba7182c101464de3274a7914f866b23e22c9
cunha.bruno <cunha.bruno@msn.com>
2020-09-20 23:17:33 +0100

    commit

--- main.py
+++ main.py
@@ -8,9 +8,8 @@ import traceback
 
 webhook = Webhook.partial(743162532356685944, 'sDHqvKb5-7dEahZZQvwLo4_X-SKDq1AAyHpR5akwt1Ms4zKAYC4QEwe7Z_YUkaiRkhKd', adapter=RequestsWebhookAdapter())
 
-wanted = [
-    "Таныоусхка",
-    "BiGuMiG"
+staff = [
+    "rifle.ak", "alex", "usekevin", "bill_san", "squidward", "country_sass"
 ]
 
 address = ('164.132.207.225', 28015)
@@ -18,9 +17,8 @@ address = ('164.132.207.225', 28015)
 async def main():
     playersFound = []
     while True:
-        print('1')
         try:
-            with valve.source.a2s.ServerQuerier(address) as query:
+            with valve.source.a2s.ServerQuerier(address, timeout=5) as query:
                 info = query.info()
                 try:
                     players = [p['name'] for p in query.players()['players']]
@@ -32,10 +30,10 @@ async def main():
                     em.set_footer(text="{player_count}/{max_players}".format(**info) + f" - {int(query.ping())}ms")
                     for player in players:
                         if player not in playersFound:
+                            content = "<@&744616236104810617>" if player.lower() in staff else ""
                             em.colour = Colour.green()
                             em.description = f"{player} joined."
-                            webhook.send(embed = em, username="{server_name}".format(**info), avatar_url='https://www.gamegrin.com/assets/games/rust/primary-image/rustlogo.jpg')
-                            print(em.description)
+                            webhook.send(content=content, embed = em, username="{server_name}".format(**info), avatar_url='https://www.gamegrin.com/assets/games/rust/primary-image/rustlogo.jpg')
                     for player in playersFound:
                         if player not in players:
                             em.colour = Colour.red()
@@ -45,7 +43,7 @@ async def main():
                 playersFound = players
         except Exception as error:
             print('\n'.join(traceback.format_exception(type(error), error, error.__traceback__)))
-        await asyncio.sleep(10)
+        await asyncio.sleep(3)
 
 loop = asyncio.get_event_loop()
 loop.create_task(main())
